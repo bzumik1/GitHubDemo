da8ef806ea7f7caf123d912a70f9bd4f1cb39821
Jakub Znamenacek <jakub.znamenacek@siemens.com>
2020-03-06 19:27:15 +0100

    Opakovane volani hello world
    Viz #7

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,10 +2,8 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello world 2");
-        System.out.println("Hello world 3");
-        System.out.println("Hello world 4");
-        System.out.println("Hello world 5");
+        for(int i=1;i<11;i++){
+            System.out.println("Hello World "+i+".");
+        }
     }
 }
